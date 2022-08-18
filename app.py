d9bb7a404e62d1773c57cf2232ca6d58e0e58a9b
Jeferson <93160555+Jeferson-Oliveiraa@users.noreply.github.com>
2022-08-18 13:02:09 -0300

    Rede_de_Academia
    Criando uma rede de Academia {Linguagem ultilizada: REST APIs com Python, Flask e banco de dados SQLite3}.

--- app.py
+++ app.py
@@ -3,14 +3,20 @@ from flask_restful import Api
 from recursos.academia import Academias,Academia
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
+@app.before_request
+def criar_banco():
+    banco.create_all()
 api.add_resource(Academias, '/academia')
 api.add_resource(Academia,'/academia/<string:academia_id>')
 
 if __name__ == '__main__':
+    from sql_alchemy import banco
+    banco.init_app(app)
     app.run(debug=True)
 
-#Criar pasta com nome modelos e dentro um arquivo __init__.py deixar vazia e criar um arquivo academia.py
 
 
---
+++ sql_alchemy.py
@@ -0,0 +1,3 @@
+from flask_sqlalchemy import SQLAlchemy
+#Teste para subir no git
+banco = SQLAlchemy()
